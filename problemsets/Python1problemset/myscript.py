217d02abccfad9be5df9f4c621c6054c5f197ecb
admin <admin@pfb06.cshl.edu>
2017-10-17 12:02:28 -0400

    added Python1problemset dir with myscript.py

---
+++ problemsets/Python1problemset/myscript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+name = str(sys.argv[1])
+color = str(sys.argv[2])
+activity = str(sys.argv[3])
+animal = str(sys.argv[4])
+
+print(name, color, activity, animal)
+
+
+ 
+
---
+++ problemsets/Python1problemset/myscript2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print('Jimmy')
+print('purple')
+print('yoga')
+print('rat')
