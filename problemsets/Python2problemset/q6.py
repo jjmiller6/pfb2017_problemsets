ae2991f4a845f63ff020917708e74b9b1830ea93
admin <admin@pfb06.cshl.edu>
2017-10-17 16:19:39 -0400

    made script for q6

---
+++ problemsets/Python2problemset/q6.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+mycompnumber = 3
+
+if mycompnumber > 10:
+	message = 'is greater than 10'
+	print(mycompnumber, message)
+elif mycompnumber < 0:
+	message = 'is less than 0'
+	print(mycompnumber, message)
+elif mycompnumber > 5:
+	message = 'is greater than 5'
+	print(mycompnumber, message)
+else:
+	message = 'doesn\'t fit any test'
+	print(mycompnumber, message)
+
+
