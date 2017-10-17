d49a5a88c952928cf4b895b3c333492e3c7f55ab
admin <admin@pfb06.cshl.edu>
2017-10-17 16:37:40 -0400

    made guess my favorite number test

---
+++ problemsets/Python2problemset/q7.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+favoritenumber = int(sys.argv[1])
+
+if favoritenumber == 4:
+	print('that\'s my favorite number!')
+else:
+	print('that isn\'t my favorite number')
+
+
+
+
