d92f05a4908ab9f40b91c3272f4654f592d76b65
admin <admin@pfb06.cshl.edu>
2017-10-19 21:14:42 -0400

    added q5

---
+++ 2problemsets/SET4/q5set4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+mylist = [101,2,15,22,95,33,2,27,72,15,52]
+
+print(mylist)
+mylist.pop() #pop removes and returns the last item
+print(mylist)
+
+print(mylist)
+rmylist = mylist.remove(15)
+print(rmylist)
